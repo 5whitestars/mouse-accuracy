59438a1e424e13c40579da1f2e317d7dc7dc6238
avischool <avi_kumar@horacemann.org>
2021-11-01 13:45:10 -0400

    It works!

--- main.js
+++ main.js
@@ -1,38 +1,49 @@
-const photos = Array.from(document.querySelectorAll("#randomdiv"));
+// const photos = Array.from(document.querySelectorAll("#randomdiv"));
 
-const scalingImage = (currentPhoto) => {
-  currentPhoto.classList.add("scaleImage");
-};
+// const scalingImage = (currentPhoto) => {
+//   currentPhoto.classList.add("scaleImage");
+// };
 
-image = document.getElementById("randomdiv");
+// image = document.getElementById("randomdiv");
 
 // image.addEventListener("click", (Event) => {
 //   scalingImage(Event.target);
 // });
 
-function RandomPosition() {
-  var posx = (Math.random() * ($(document).width() - 100)).toFixed();
-  var posy = (Math.random() * ($(document).height() - 100)).toFixed();
+function createDiv() {
+  let image = document.createElement("div");
+  let parent = document.querySelector("#parentdiv");
+  image.style.width = "100px";
+  image.style.height = "100px";
+  image.style.background = "red";
+  var posx = Math.random() * 100;
+  var posy = Math.random() * 100;
   image.style.position = "absolute";
-  image.style.left = posx + "px";
-  image.style.top = posy + "px";
-  var el = document.getElementById("photo");
-  el.style.animation = "none";
-  el.offsetHeight; /* trigger reflow */
-  el.style.animation = null;
-  scalingImage(image);
+  image.style.left = posx + "vw";
+  image.style.top = posy + "vh";
+  parent.appendChild(image);
+  image.style.animation = "spin2 4s forwards";
 }
 
-buttonclicked = false;
-function myFunction() {
-  buttonclicked = true;
-}
+// function RandomPosition() {
+//   var posx = (Math.random() * ($(document).width() - 100)).toFixed();
+//   var posy = (Math.random() * ($(document).height() - 100)).toFixed();
+//   image.style.position = "absolute";
+//   image.style.left = posx + "px";
+//   image.style.top = posy + "px";
+//   var el = document.getElementById("photo");
+//   el.style.animation = "none";
+//   el.offsetHeight; /* trigger reflow */
+//   el.style.animation = null;
+//   scalingImage(image);
+// }
 
-RandomPosition();
+// buttonclicked = false;
+// function myFunction() {
+//   buttonclicked = true;
+// }
+
+createDiv();
 const interval = setInterval(function () {
-  if (buttonclicked == true) {
-    image.style.display = "none";
-    clearInterval(interval);
-  }
-  RandomPosition();
+  createDiv();
 }, 2000);
