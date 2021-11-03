835ecce07008a4382bbc3dfad18a7bc33e6bd5b1
Diane <dianekim120@gmail.com>
2021-11-03 13:01:28 -0400

    clean up code

--- main.js
+++ main.js
@@ -1,15 +1,3 @@
-// const photos = Array.from(document.querySelectorAll("#randomdiv"));
-
-// const scalingImage = (currentPhoto) => {
-//   currentPhoto.classList.add("scaleImage");
-// };
-
-// image = document.getElementById("randomdiv");
-
-// image.addEventListener("click", (Event) => {
-//   scalingImage(Event.target);
-// });
-
 function createDiv() {
   let image = document.createElement("div");
   let parent = document.querySelector("#parentdiv");
@@ -24,24 +12,6 @@ function createDiv() {
   parent.appendChild(image);
   image.style.animation = "spin2 4s forwards";
 }
-
-// function RandomPosition() {
-//   var posx = (Math.random() * ($(document).width() - 100)).toFixed();
-//   var posy = (Math.random() * ($(document).height() - 100)).toFixed();
-//   image.style.position = "absolute";
-//   image.style.left = posx + "px";
-//   image.style.top = posy + "px";
-//   var el = document.getElementById("photo");
-//   el.style.animation = "none";
-//   el.offsetHeight; /* trigger reflow */
-//   el.style.animation = null;
-//   scalingImage(image);
-// }
-
-// buttonclicked = false;
-// function myFunction() {
-//   buttonclicked = true;
-// }
 
 createDiv();
 const interval = setInterval(function () {
